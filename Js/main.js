0097c5d05f2d5a855f368713aca094ceb4d4e249
Rocio Yovera <rocioyovera@ gmail.com>
2016-12-27 15:30:14 -0500

    agregué el link

--- Js/main.js
+++ Js/main.js
@@ -1,31 +1,12 @@
 function calcular()
 {	
-	var numero = document.getElementById("numer1").value;
-	var diferencia = document.getElementById("diff").value;
-	
+	var number1 = document.getElementById("number1").value;
+	var diff = document.getElementById("diff");
 
+	if(number1>21)
 
-	if(numer1>21)
-	{	
-		document.getElementById("salida").innerHTML = "Muy bien Squad!";
-	}else{
-		document.getElementById("salida").innerHTML = "Pueden hacerlo mejor";
-	}
 
 }
 
 
 
-function evaluar ()
-{
-	var aSmile = document.getElementById("mono_a").checked;
-	var bSmile = document.getElementById("mono_b").checked;
-	var salida = document.getElementById("salida");
-
-	if(aSmile == bSmile)
-	{
-		salida.innerHTML = "1";
-	}else{
-		salida.innerHTML = "0";
-	}
-}
